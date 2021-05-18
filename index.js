473219a78794829993508864b0f2f3a337c1c226
Andrew Masih <armasih@outlook.com>
2021-05-18 21:09:20 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let myString = "Creativity";
+              /*0123456789 */
+
+/* It excluded first character at the zeroth position
+and seventh character and everything after it*/
+myString.slice(1, 7);
